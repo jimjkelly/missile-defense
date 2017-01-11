522812f067aac89a50e0a719ea5387f0dca603f2
Jim Kelly <pthread1981@gmail.com>
2017-01-11 02:22:23 +0100

    Revert "Fix P(0) Calculation"
    This reverts commit 64c89499f96490482789ccd8ae13165bc639d695.

--- tests/test-calculations.js
+++ tests/test-calculations.js
@@ -61,7 +61,7 @@ test('test p0 with notional offense', () => {
         {
             hardness: 10
         }
-    )).toBeCloseTo(0.03609999999999998, 12)
+    )).toBeCloseTo(0.15389999999999995, 12)
 });
 
 test('test p0 with ground burst offense', () => {
@@ -88,5 +88,5 @@ test('test p0 with ground burst offense', () => {
         {
             hardness: 10
         }
-    )).toBeCloseTo(0.01899999999999999, 12)
+    )).toBeCloseTo(0.17099999999999996, 12)
 });
