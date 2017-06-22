2e01b2485a0fd8617913fdf2715e8b69afdfc650
Jim Kelly <pthread1981@gmail.com>
2017-06-22 15:39:10 -0400

    Add error logging for bots

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,36 @@ it to our Redux store.
 
 */
 
+/* eslint-disable */
+if (/bot|googlebot|crawler|spider|robot|crawling/i.test(navigator.userAgent)) {
+	window.onerror = function (message, url, lineNo, colNo, error) {
+    console.log(arguments);
+
+    let container = document.createElement('div');
+
+    container.style.color = 'red';
+    container.style.position = 'fixed';
+    container.style.background = '#eee';
+    container.style.padding = '2em';
+    container.style.top = '1em';
+    container.style.left = '1em';
+
+    let msg = document.createElement('pre');
+    msg.innerText = [
+      'Message: ' + message,
+      'URL: ' + url,
+      'Line: ' + lineNo,
+      'Column: ' + colNo,
+      'Stack: ' + (error && error.stack)
+    ].join('\n');
+
+    container.appendChild(msg);
+
+    document.body.appendChild(container);
+  };
+}
+/* eslint-enable */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
@@ -18,6 +48,6 @@ import { store } from './store';
 const Page = connect(state=> state || {})(PageControl);
 
 ReactDOM.render(
-	<Page store={store} />,
-	document.getElementById('react-root')
+  <Page store={store} />,
+  document.getElementById('react-root')
 );
